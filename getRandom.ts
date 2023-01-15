608b85a451b898e75146b1b38d064484d7298547
Th-release.cth <84012697+th-release@users.noreply.github.com>
2023-01-15 20:35:02 +0900

    feat: Create `getRandom.tsx`

---
+++ getRandom.ts
@@ -0,0 +1,31 @@
+function getRandom = (type_: string, length: number) => {
+    let result = "";
+    const type = type_.toLowerCase();
+    length = length ? length : 32;
+    let characters = "0123456789";
+    if (type === 'number' || type === 'numbers') {
+      characters = "0123456789";
+      for (let i = 0; i < length; i++) {
+        result += characters[Math.floor(Math.random() * characters.length)];
+      }
+      return result;
+    } else if (type === 'alphabet' || type === 'alphabets') { 
+      characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      for (let i = 0; i < length; i++) {
+        result += characters[Math.floor(Math.random() * characters.length)];
+      }
+      return result;
+    } else if (type === 'alphanumeric' || type === 'all') {
+      characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      for (let i = 0; i < length; i++) {
+        result += characters[Math.floor(Math.random() * characters.length)];
+      }
+      return result;
+    } else {
+      characters = type;
+      for (let i = 0; i < length; i++) {
+        result += characters[Math.floor(Math.random() * characters.length)];
+      }
+      return result;
+    }
+  }
