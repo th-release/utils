616373f90ef4de8818bb9af56e56f33c2879a117
cth.release <84012697+th-release@users.noreply.github.com>
2023-09-20 12:05:25 +0900

    Create WeightedRandomPicker.ts

---
+++ WeightedRandomPicker.ts
@@ -0,0 +1,25 @@
+function getRandomWeightedItem(items: { name: string; weight: number }[]): string {
+  while (true) {
+    const totalWeight = items.reduce((sum, item) => sum + item.weight, 0);
+
+    const randomWeight = Math.random() * totalWeight;
+
+    let accumulatedWeight = 0;
+    for (const item of items) {
+      accumulatedWeight += item.weight;
+      if (randomWeight <= accumulatedWeight) {
+        return item.name;
+      }
+    }
+  }
+}
+
+// test ------------------------------------------
+// const items = [
+//   { name: "아이템1", weight: 0.03 },
+//   { name: "아이템2", weight: 5 },
+//   { name: "아이템3", weight: 6 },
+// ];
+
+// const selected = getRandomWeightedItem(items);
+// console.log(`선택된 아이템: ${selected}`);
