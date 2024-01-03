fdc3f74439602ad51b22161f3eb895fd8e323fd1
th-release <cth.release@gmail.com>
2024-01-03 17:37:22 +0900

    feat: Added Hashs

---
+++ go/sha512.go
@@ -0,0 +1,20 @@
+package main
+
+// go get golang.org/x/crypto/sha3
+
+import (
+	"encoding/hex"
+
+	"golang.org/x/crypto/sha3"
+)
+
+func hash(input string) (string, error) {
+	hasher := sha3.New512()
+	hasher.Write([]byte(input))
+	hashBytes := hasher.Sum(nil)
+
+	// 16진수 문자열로 변환
+	hashString := hex.EncodeToString(hashBytes)
+
+	return hashString, nil
+}
