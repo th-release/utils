04710d1ecea58556f10534e10d92fc6fd2d69ed9
cth.release <84012697+th-release@users.noreply.github.com>
2024-01-18 13:30:13 +0900

    feat: Works even when the value is a number

--- typescript/base64.ts
+++ typescript/base64.ts
@@ -1,9 +1,12 @@
-function base64Encode(str: string): string {
-    let buff = Buffer.from(str, 'utf-8');
+function base64Encode(input: string | number): string {
+    const str = input.toString();
+    const buff = Buffer.from(str, 'utf-8');
     return buff.toString('base64');
 }
 
-function base64Decode(base64: string): string {
-    let buff = Buffer.from(base64, 'base64');
-    return buff.toString('utf-8');
+function base64Decode(base64: string): string | number {
+    const buff = Buffer.from(base64, 'base64');
+    const str = buff.toString('utf-8');
+    return isNaN(Number(str)) ? str : Number(str);
 }
+
