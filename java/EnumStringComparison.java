8df448550472b7187840b35fe30d7f40983a8003
cth.release <84012697+th-release@users.noreply.github.com>
2024-03-19 15:03:12 +0900

    fix: Update EnumStringComparison.java

--- java/EnumStringComparison.java
+++ java/EnumStringComparison.java
@@ -1,12 +1,16 @@
 package com.threlease.base.utils;
 
 public class EnumStringComparison {
-    public boolean compareEnumString(String str, Enum<?> value) {
-        if (value == null || str == null) {
+    public static <T extends Enum<T>> boolean compareEnumString(String str, Class<T> enumClass) {
+        if (enumClass == null || str == null) {
             return false;
         }
 
-        // 문자열과 열거형(enum) 값 비교
-        return str.equals(value.toString());
+        for (T enumValue : enumClass.getEnumConstants()) {
+            if (str.equals(enumValue.toString())) {
+                return true; // 문자열과 일치하는 Enum 값을 찾으면 true 반환
+            }
+        }
+        return false; // 일치하는 Enum 값이 없으면 false 반환
     }
 }
