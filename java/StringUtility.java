3b1741142151d06ea7cedbddc982db398b58d39f
cth.release <84012697+th-release@users.noreply.github.com>
2024-03-25 17:57:37 +0900

    feat: Create StringUtility.java

---
+++ java/StringUtility.java
@@ -0,0 +1,29 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.*;
+
+public class StringUtility {
+    public Map<String, String> stringToJson(String jsonString) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(jsonString, Map.class);
+    }
+
+    public String mapToJson(Map<String, String> map) {
+        StringBuilder json = new StringBuilder("{");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            json.append("\"").append(entry.getKey()).append("\":\"").append(entry.getValue()).append("\",");
+        }
+        json.deleteCharAt(json.length() - 1);
+        json.append("}");
+        return json.toString();
+    }
+
+    public String mapToParams(Map<String, String> maps) {
+        StringBuilder paramString = new StringBuilder();
+        for (Map.Entry<String, String> entry : maps.entrySet()) {
+            paramString.append(entry.getKey()).append("=").append(entry.getValue()).append("&");
+        }
+
+        return paramString.toString();
+    }
+}
