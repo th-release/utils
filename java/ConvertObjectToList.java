6f7cda555c740d1913425ff8c096c23b7840d841
cth.release <84012697+th-release@users.noreply.github.com>
2024-03-25 17:55:19 +0900

    feat: Create ConvertObjectToList.java

---
+++ java/ConvertObjectToList.java
@@ -0,0 +1,11 @@
+class ConvertObjectToList {
+    public static List<?> convertObjectToList(Object obj) {
+        List<?> list = new ArrayList<>();
+        if (obj.getClass().isArray()) {
+            list = Arrays.asList((Object[])obj);
+        } else if (obj instanceof Collection) {
+            list = new ArrayList<>((Collection<?>)obj);
+        }
+        return list;
+    } 
+}
