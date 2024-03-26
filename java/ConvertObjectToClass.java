585e6a037bdfccb40900cd6863c6bfd92d89024b
cth.release <84012697+th-release@users.noreply.github.com>
2024-03-27 00:05:45 +0900

    Delete:  java/ConvertObjectToClass.java

--- java/ConvertObjectToClass.java
+++
@@ -1,8 +0,0 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public class ConvertObjectToClass {
-    public <T> T convertObjectToClass(Object data, Class<T> clazz) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.convertValue(data, clazz);
-    }
-}
