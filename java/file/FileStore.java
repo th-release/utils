d38ed319baaf471065cae2a2264a58640cecd8c4
cth.release <84012697+th-release@users.noreply.github.com>
2024-08-06 16:01:02 +0900

    feat: Added fileExist, readFile function

--- java/file/FileStore.java
+++ java/file/FileStore.java
@@ -1,8 +1,12 @@
 import com.threlease.base.utils.Failable;
 import org.springframework.web.multipart.MultipartFile;
+import com.threlease.base.utils.Failable;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 public class FileStore {
@@ -10,7 +14,7 @@ public class FileStore {
 
     public FileStore(String fileDir) {
         String rootPath = System.getProperty("user.dir");
-        this.fileDir = rootPath + fileDir;
+        this.fileDir = rootPath + File.separator + fileDir;
     }
 
     public String getFullPath(String filename) { return fileDir + File.separator + filename; }
@@ -25,7 +29,13 @@ public class FileStore {
             return Failable.error(e.getMessage());
         }
     }
-    
+
+    public boolean fileExist(String filename) {
+        Path path = Paths.get(this.fileDir + File.separator + filename);
+
+        return !Files.exists(path);
+    }
+
     public Failable<UploadFile, String> storeFile(MultipartFile multipartFile) {
 
         if(multipartFile.isEmpty()) {
@@ -37,10 +47,10 @@ public class FileStore {
         String storeFilename = UUID.randomUUID() + "." + extractExt(originalFilename);
 
         if (!extractExt(originalFilename).equals("jpg") &&
-            !extractExt(originalFilename).equals("png") &&
-            !extractExt(originalFilename).equals("jpeg") &&
-            !extractExt(originalFilename).equals("gif") &&
-            !extractExt(originalFilename).equals("bmp")
+                !extractExt(originalFilename).equals("png") &&
+                !extractExt(originalFilename).equals("jpeg") &&
+                !extractExt(originalFilename).equals("gif") &&
+                !extractExt(originalFilename).equals("bmp")
         ) return Failable.error("이미지만 업로드해주세요.");
 
         try {
@@ -49,6 +59,17 @@ public class FileStore {
             return Failable.success(new UploadFile(originalFilename, storeFilename));
         } catch (IOException e) {
             return Failable.error(e.getMessage());
+        }
+    }
+
+    public Failable<String, String> readFile(String filename) {
+        if (!fileExist(filename))
+            return Failable.error("File Not Exist");
+
+        try {
+            return Failable.success(new String(Files.readAllBytes(Paths.get(this.fileDir + File.separator + filename))));
+        } catch (IOException e) {
+            return Failable.error("Error reading file: " + e.getMessage());
         }
     }
 
