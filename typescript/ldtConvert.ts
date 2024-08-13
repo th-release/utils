0bc65780486c919df8250ea8e4d64b481ef51748
cth.release <84012697+th-release@users.noreply.github.com>
2024-08-13 10:04:53 +0900

    feat: format

--- typescript/ldtConvert.ts
+++ typescript/ldtConvert.ts
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-export function ldtConvert(arr: any[]) {
+export function ldtConvert(arr: any[], foramt: string) {
   const date = new Date();
 
   date.setFullYear(arr[0], arr[1], arr[2])
@@ -8,5 +8,5 @@ export function ldtConvert(arr: any[]) {
   date.setHours(arr[3])
   date.setMinutes(arr[4])
 
-  return moment(date).format('YYYY-MM-DD HH:mm')
+  return moment(date).format(foramt)
 }
